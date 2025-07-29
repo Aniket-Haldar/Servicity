a73c1bd8863688f604eb16cc2bb642a543aa89d1
Aniket-Haldar <aniket.haldar05@gmail.com>
2025-07-29 23:56:59 +0530

    Change in Dockerfile

--- frontend/js/onboarding.js
+++ frontend/js/onboarding.js
@@ -42,8 +42,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Check if user is logged in
-    const token = getCookie('token');
-    if (!token) {
-        window.location.href = 'http://127.0.0.1:3000/auth/google/login';
-    }
+    
 });
